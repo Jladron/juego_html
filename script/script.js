1c28094af52dae67973609cd3704e3c38b32c3e5
jladron <jladron@hotmail.es>
2020-08-12 10:51:11 +0200

    comit 5

---
+++ script/script.js
@@ -0,0 +1,31 @@
+var personaje = document.getElementById("personaje");
+
+document.addEventListener("click", salto);
+
+function salto() {
+
+    if (personaje.classList == "animacion") {
+        return;
+    }
+
+    personaje.classList.add("animacion");
+    setTimeout(eliminarSalto, 300);
+}
+
+function eliminarSalto() {
+    personaje.classList.remove("animacion");
+}
+
+var block = document.getElementById("block");
+
+function comprobarMuerte() {
+    let personajeTop = parseInt(window.getComputedStyle(personaje).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+
+    if (blockLeft < 20 && blockLeft > -20 && personajeTop >= 130) {
+        alert("Game Over");
+    }
+}
+
+
+setInterval(checkDead, 10);
