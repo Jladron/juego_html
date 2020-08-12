20639b2a40f1c328e1204cf993007ca6084436b2
jladron <jladron@hotmail.es>
2020-08-12 11:06:52 +0200

    comit 7

--- script/script.js
+++ script/script.js
@@ -1,31 +1,21 @@
-var personaje = document.getElementById("personaje");
-
-document.addEventListener("click", salto);
-
-function salto() {
-
-    if (personaje.classList == "animacion") {
-        return;
-    }
-
-    personaje.classList.add("animacion");
-    setTimeout(eliminarSalto, 300);
-}
-
-function eliminarSalto() {
-    personaje.classList.remove("animacion");
+var character = document.getElementById("character");
+document.addEventListener("click", jump);
+function jump() {
+    if (character.classList == "animate") { return; }
+    character.classList.add("animate");
+    setTimeout(removeJump, 300); //300ms = length of animation
+};
+function removeJump() {
+    character.classList.remove("animate");
 }
 
 var block = document.getElementById("block");
-
-function comprobarMuerte() {
-    let personajeTop = parseInt(window.getComputedStyle(personaje).getPropertyValue("top"));
+function checkDead() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-
-    if (blockLeft < 20 && blockLeft > -20 && personajeTop >= 130) {
-        alert("Game Over");
+    if (blockLeft < 20 && blockLeft > -20 && characterTop >= 130) {
+        alert("Game over");
     }
 }
 
-
-setInterval(comprobarMuerte, 10);
+setInterval(checkDead, 10);
